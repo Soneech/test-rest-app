ad23e3c2bb7cd7c4547e1a8596341058dc3c5cc0
Soneech <sonya.patrina@yandex.ru>
2023-09-20 13:13:05 +0300

    Initial commit

---
+++ src/test/java/org/soneech/springcourse/TestRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soneech.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
